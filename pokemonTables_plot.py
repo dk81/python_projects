8d37d5161ae23c8516853a17f38dccc4cbb1c2a4
unknown <dku.8691@gmail.com>
2018-07-19 11:10:44 -0400

    Using Python to generate a Pokemon type tables plot.

---
+++ pokemonTables_plot.py
@@ -0,0 +1,53 @@
+# Heatmap Plot On Correlation Matrix
+# Pokemon Types Table Current Generation
+
+### A Pokemon Types Table Plot In Python
+### Seaborn Approach:
+# https://stackoverflow.com/questions/33158075/custom-annotation-seaborn-heatmap
+# https://stackoverflow.com/questions/40734343/artificial-tick-labels-for-seaborn-heatmaps
+# Pokemon Type Table Reference: https://img.pokemondb.net/images/typechart.png
+# Reference: https://stackoverflow.com/questions/533905/get-the-cartesian-product-of-a-series-of-lists
+
+
+
+import seaborn as sns
+import numpy as np
+from matplotlib import pyplot as plt
+
+pokemon_types = ["Normal", "Fire", "Water", "Electric", "Grass", "Ice",
+                 "Fighting", "Poison", "Ground", "Flying", "Psychic",
+                 "Bug", "Rock", "Ghost", "Dragon", "Dark", "Steel", "Fairy"]
+
+# A 2 Dimenstional Numpy Array Of Damage Multipliers For Attacking Pokemon:
+    
+damage_array = np.array([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1/2, 0, 1, 1, 1/2, 1],
+                    [1, 1/2, 1/2, 1, 2, 2, 1, 1, 1, 1, 1, 2, 1/2, 1, 1/2, 1, 2, 1],
+                    [1, 2, 1/2, 1, 1/2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1/2, 1, 1, 1],
+                    [1, 1, 2, 1/2, 1/2, 1, 1, 1, 0, 2, 1, 1, 1, 1, 1/2, 1, 1, 1],
+                    [1, 1/2, 2, 1, 1/2, 1, 1, 1/2, 2, 1/2, 1, 1/2, 2, 1, 1/2, 1, 1/2, 1],
+                    [1, 1/2, 1/2, 1, 2, 1/2, 1, 1, 2, 2, 1, 1, 1, 1, 2, 1, 1/2, 1],
+                    [2, 1, 1, 1, 1, 2, 1, 1/2, 1, 1/2, 1/2, 1/2, 2, 0, 1, 2, 2, 1/2],
+                    [1, 1, 1, 1, 2, 1, 1, 1/2, 1/2, 1, 1, 1, 1/2, 1/2, 1, 1, 0, 2],
+                    [1, 2, 1, 2, 1/2, 1, 1, 2, 1, 0, 1, 1/2, 2, 1, 1, 1, 2, 1],
+                    [1, 1, 1, 1/2, 2, 1, 2, 1, 1, 1, 1, 2, 1/2, 1, 1, 1, 1/2, 1],
+                    [1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1/2, 1, 1, 1, 1, 0, 1/2, 1],
+                    [1, 1/2, 1, 1, 2, 1, 1/2, 1/2, 1, 1/2, 2, 1, 1, 1/2, 1, 2, 1/2, 1/2],
+                    [1, 2, 1, 1, 1, 2, 1/2, 1, 1/2, 2, 1, 2, 1, 1, 1, 1, 1/2, 1],
+                    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1/2, 1, 1],
+                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1/2, 0],
+                    [1, 1, 1, 1, 1, 1, 1/2, 1, 1, 1, 2, 1, 1, 2, 1, 1/2, 1, 1/2],
+                    [1, 1/2, 1/2, 1/2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1/2, 2],
+                    [1, 1/2, 1, 1, 1, 1, 2, 1/2, 1, 1, 1, 1, 1, 1, 2, 2, 1/2, 1]])
+
+fig, ax = plt.subplots()
+ax = sns.heatmap(damage_array, annot = damage_array, fmt = '', cmap="Greens",
+                 xticklabels = pokemon_types, yticklabels = pokemon_types)
+
+ax.set_xlabel("\n\n Defending Pokémon", size = 14)
+ax.set_ylabel("Attacking Pokémon \n\n", size = 14)
+plt.title("A Pokémon Type Chart - Generation 6 \n\n", size = 18, color = "#b30000") 
+plt.show(fig)
+
+
+                 
+
